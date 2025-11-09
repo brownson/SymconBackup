dbb61ff58807f276cf2977c3889ea266803f59f6
Michael Maroszek <paresy@gmail.com>
2025-11-09 14:27:14 +0100

    bump phpseclib from 3.0.19 to 3.0.47

--- libs/vendor/composer/autoload_real.php
+++ libs/vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit325d0df57bc49a71c320235f3e12b987
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit325d0df57bc49a71c320235f3e12b987::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
